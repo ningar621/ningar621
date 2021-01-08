c5f8e21848958e62b1dfc33641cff97cf914ff1d
郭晓宁 <guoxiaoning@cmcm.com>
2021-01-08 15:48:34 +0800

    楼层指引

--- js/two_floor.js
+++ js/two_floor.js
@@ -148,7 +148,7 @@ function canvasMap(){
     // spotLeft1.position.set( 4955.21, -600.01, -91.46 );
     // two_1.add(spotLeft1);
     
-    two_1.add(tcenter1,spotSingapore1,spotKualaLumpur1,spotLatinAmerica1,tspotRight1,spotRight1_2,spotLeft1,spotLeft1_2,spotBangkok,tspotWC1,spotNewDelhi);
+    two_1.add(tcenter1,spotSingapore1,spotKualaLumpur1,spotLatinAmerica1,tspotRight1,spotRight1_2,spotLeft1_2,spotBangkok,tspotWC1,spotNewDelhi);
     //二楼场景2
     tcenter2 = new THREE.Mesh( new THREE.BoxGeometry(0, 0, 0), new THREE.MeshNormalMaterial() );
     tcenter2.position.set( 389.21, 51.22, 4977.59 );
